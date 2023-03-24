938fd9c948475ad9b5bb170c6fbb15b05c3cfd94
mrgick <mrgickcool@gmail.com>
2023-03-24 21:35:50 +0300

    upgrade django to 4.1.7

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ Definition of forms.
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class BootstrapAuthenticationForm(AuthenticationForm):
--- table_games/settings.py
+++ table_games/settings.py
@@ -98,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = "ru-ru"
 TIME_ZONE = "UTC"
 USE_I18N = True
-USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)

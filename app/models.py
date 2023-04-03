f6a9ff295e9ee3b70c5900638a8b1e019e07d6b1
mrgick <mrgickcool@gmail.com>
2023-04-03 14:30:51 +0300

    Lab 7. Add blog model.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,35 @@
 """
 Definition of models.
 """
+from datetime import datetime
 
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(
+        max_length=100, unique_for_date="posted", verbose_name="Заголовок"
+    )
+    description = models.TextField(verbose_name="Краткое содержание")
+    content = models.TextField(verbose_name="Полное содержание")
+    posted = models.DateTimeField(
+        default=datetime.now(), db_index=True, verbose_name="Опубликована"
+    )
+    author = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.SET_NULL, verbose_name="Автор"
+    )
+
+    def get_absolute_url(self):
+        """Метод возвращает строку с URL-адресом записи."""
+        return reverse("blogpost", args=[str(self.id)])
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "Posts"
+        ordering = ["-posted"]
+        verbose_name = "статья блога"
+        verbose_name_plural = "статьи блога"

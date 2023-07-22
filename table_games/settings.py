b9c905d208d11e2f70600f1e9d512fcbb7871e91
mrgick <mrgickcool@gmail.com>
2023-07-22 17:30:44 +0300

    Update settings

--- table_games/settings.py
+++ table_games/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = "table_games.wsgi.application"
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     "default": {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'HOST': os.getenv('DB_HOST'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'PORT': os.getenv('DB_PORT'),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
